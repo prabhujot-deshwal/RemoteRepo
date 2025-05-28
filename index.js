5ace8f7a2807e6e3ecb7a227a9df7c46081956ff
Prabhujot_Deshwal <prabhujotdeshwalcse@gmail.com>
2025-05-28 15:39:28 +0530

    HEY

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
-// Setup
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Sample sweet data
 let sweets = ['Ladoo', 'Barfi', 'Gulab Jamun'];
 
-// Routes
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -25,6 +25,4 @@ app.post('/add', (req, res) => {
   res.redirect('/sweets');
 });
 
-app.listen(3000, () => {
-  console.log('Sweet shop running at http://localhost:3000');
-});
+module.exports = app;
